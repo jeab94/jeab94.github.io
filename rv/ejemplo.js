fac52112728f36c1b8c023d53f761cafd213d563
jeab94 <123456789manu@hotmail.es>
2016-08-16 15:20:49 -0500

    Update ejemplo.js

--- rv/ejemplo.js
+++ rv/ejemplo.js
@@ -1,4 +1,4 @@
 var miCanvas = document.getElementById("miCanvas");
 var contexto = miCanvas.getContext("2d");
-conexto.fillStyle = "red";
+contexto.fillStyle = "red";
 contexto.fillRect(30, 30, 50, 50);
