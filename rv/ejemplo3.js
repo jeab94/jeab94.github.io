0ecde13a43fb2da9735d8234e4c1ce108ca0a267
jeab94 <123456789manu@hotmail.es>
2016-08-16 23:03:43 -0500

    Update ejemplo3.js

--- rv/ejemplo3.js
+++ rv/ejemplo3.js
@@ -1,12 +1,10 @@
 var miCanvas1 = document.getElementById("miCanvas1");
 var contexto1 = miCanvas1.getContext("2d");
+var contexto2 = miCanvas1.getContext("2d");
+var contexto3 = miCanvas1.getContext("2d");
 contexto1.fillStyle = "red";
 contexto1.fillRect(0, 0, 50, 50);
-var miCanvas2 = document.getElementById("micanvas2");
-var contexto2 = miCanvas2.getContext("2d");
 contexto2.fillStyle = "green";
 contexto2.fillRect(900, 0, 50, 50);
-var miCanvas3 = document.getElementById("miCanvas3");
-var contexto3 = miCanvas3.getContext("2d");
 contexto3.fillStyle = "blue";
 contexto3.fillRect(1800, 0, 50, 50);
