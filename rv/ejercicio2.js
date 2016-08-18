2d1dd2bd92c24260cd0dacf3c39946247b5e8bba
jeab94 <123456789manu@hotmail.es>
2016-08-18 15:57:24 -0500

    Update ejercicio2.js

--- rv/ejercicio2.js
+++ rv/ejercicio2.js
@@ -4,10 +4,14 @@ camara.position.z = 5;  //Las cámaras heredan de un objeto tipo Object3D() con 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95); //window da las dimensiones internas de la ventana del navegador (autoescalado al 95%)
 document.body.appendChild(renderizador.domElement); //Inserta un nodo al Body 
-var forma = new THREE.TetrahedronGeometry(1,0);
-var material = new THREE.MeshNormalMaterial(); //el de default sería MeshBasicMaterial
-var tetraedro = new THREE.Mesh(forma,material); //Cosntructor Mesh()
-tetraedro.rotateX(-Math.PI/4); //Rota 45° en X
+
+var forma1 = new THREE.TetrahedronGeometry(1,0);
+var material1 = new THREE.MeshNormalMaterial(); //el de default sería MeshBasicMaterial
+var tetraedro = new THREE.Mesh(forma1,material1); //Cosntructor Mesh()
+//tetraedro.rotateX(-Math.PI/4); //Rota 45° en X
 tetraedro.rotateY(-Math.PI/4); //Rota 45° en Y
 escena.add(tetraedro);
+
+
+
 renderizador.render(escena,camara);
