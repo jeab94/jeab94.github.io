18414c52812bf4da142fff575b2269cf05fb8315
jeab94 <123456789manu@hotmail.es>
2016-08-23 14:52:52 -0500

    Update ejercicio2.js

--- rv/ejercicio2.js
+++ rv/ejercicio2.js
@@ -12,7 +12,7 @@ tetraedro.rotateX(-Math.PI/4); //Rota 45° en X
 tetraedro.rotateY(-Math.PI/4); //Rota 45° en Y
 escena.add(tetraedro);
 
-var geometry = new THREE.CylinderBufferGeometry( 1, 1, 500, 32 );
+var geometry = new THREE.CylinderBufferGeometry( 1, 1, 100, 32 );
 var material2 = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
 var cylinder = new THREE.Mesh( geometry, material2 );
 escena.add( cylinder );
