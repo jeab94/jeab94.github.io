a7682c4d838e10c5c37537785e1706f53f8d682e
jeab94 <123456789manu@hotmail.es>
2016-08-23 15:27:28 -0500

    Geometría Mallas Poligonales

---
+++ rv/ejemplo-geometry.js
@@ -0,0 +1,35 @@
+var forma = new THREE.Geometry(); //Nueva variable tipo Geometría
+
+//forma.vertices=[]
+forma.vertices.push( new THREE.Vector3( 1, 0, 1 ) ); //Enfocamos un valor de  un vector de 3 Vector3(x0,y0,z0) Vertice no.0 
+forma.vertices.push( new THREE.Vector3( 1, 0,-1 ) ); //Vertice no.1
+forma.vertices.push( new THREE.Vector3(-1, 0,-1 ) ); //Vertice no.2
+forma.vertices.push( new THREE.Vector3(-1, 0,1 ) ); //Vertice no.3
+forma.vertices.push( new THREE.Vector3( 0, 1, 0 ) ); //Vertice no.4
+
+forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); //Cara formada por tres vertices
+forma.faces.push( new THREE.Face3( 3, 1, 0 ) );
+forma.faces.push( new THREE.Face3( 3, 0, 4 ) );
+forma.faces.push( new THREE.Face3( 0, 1, 4 ) );
+forma.faces.push( new THREE.Face3( 1, 2, 4 ) );
+forma.faces.push( new THREE.Face3( 2, 3, 4 ) );
+
+forma.computeBoundingSphere(); //Esfera de menor tamaño que acota al objeto
+forma.computeFaceNormals(); //Se computan las normales de las caras
+
+var material = new THREE.MeshNormalMaterial();
+
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX(Math.PI/4);
+
+var escena = new THREE.Scene();
+escena.add( malla );
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95,
+                      window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara);
