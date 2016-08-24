291f6e62195dd105228f75cdff0575a5d2ca5e8c
jeab94 <123456789manu@hotmail.es>
2016-08-23 23:32:01 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -6,7 +6,7 @@ forma.vertices.push( new THREE.Vector3(-1, 0,-1 ) ); //Vertice no.2
 forma.vertices.push( new THREE.Vector3(-1, 0,1 ) ); //Vertice no.3
 forma.vertices.push( new THREE.Vector3( 0, 1, 0 ) ); //Vertice no.4
 
-forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); //Cara formada por tres vertices
+forma.faces.push( new THREE.Face3( 3, 1, 2 ) ); //Cara formada por tres vertices
 
 
 
