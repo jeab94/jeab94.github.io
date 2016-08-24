840b2329ebc1661a0eab2cb154df7ed12c3c383e
jeab94 <123456789manu@hotmail.es>
2016-08-23 23:28:07 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -1,22 +1,36 @@
 var forma = new THREE.Geometry(); //Nueva variable tipo Geometría
 
-//forma.vertices=[]
 forma.vertices.push( new THREE.Vector3( 1, 0, 1 ) ); //Enfocamos un valor de  un vector de 3 Vector3(x0,y0,z0) Vertice no.0 
 forma.vertices.push( new THREE.Vector3( 1, 0,-1 ) ); //Vertice no.1
 forma.vertices.push( new THREE.Vector3(-1, 0,-1 ) ); //Vertice no.2
-forma.vertices.push( new THREE.Vector3(-1, 0, 1 ) ); //Vertice no.3
-forma.vertices.push( new THREE.Vector3( 1, 2, 1 ) ); //Vertice no.4 
-forma.vertices.push( new THREE.Vector3( 1, 2,-1 ) ); //Vertice no.5
-forma.vertices.push( new THREE.Vector3(-1, 2,-1 ) ); //Vertice no.6
-forma.vertices.push( new THREE.Vector3(-1, 2, 1 ) ); //Vertice no.7
-
+forma.vertices.push( new THREE.Vector3(-1, 0,1 ) ); //Vertice no.3
+forma.vertices.push( new THREE.Vector3( 0, 1, 0 ) ); //Vertice no.4
 
 forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); //Cara formada por tres vertices
 forma.faces.push( new THREE.Face3( 3, 1, 0 ) );
-forma.faces.push( new THREE.Face3( 7, 6, 5 ) );
-forma.faces.push( new THREE.Face3( 7, 5, 4 ) );
-forma.faces.push( new THREE.Face3( 5, 4, 1 ) );
-forma.faces.push( new THREE.Face3( 6, 5, 2 ) );
+forma.faces.push( new THREE.Face3( 3, 0, 4 ) );
+forma.faces.push( new THREE.Face3( 0, 1, 4 ) );
+forma.faces.push( new THREE.Face3( 1, 2, 4 ) );
+forma.faces.push( new THREE.Face3( 2, 3, 4 ) );
+
+
+////forma.vertices=[]
+//forma.vertices.push( new THREE.Vector3( 1, 0, 1 ) ); //Enfocamos un valor de  un vector de 3 Vector3(x0,y0,z0) Vertice no.0 
+//forma.vertices.push( new THREE.Vector3( 1, 0,-1 ) ); //Vertice no.1
+//forma.vertices.push( new THREE.Vector3(-1, 0,-1 ) ); //Vertice no.2
+//forma.vertices.push( new THREE.Vector3(-1, 0, 1 ) ); //Vertice no.3
+//forma.vertices.push( new THREE.Vector3( 1, 2, 1 ) ); //Vertice no.4 
+//forma.vertices.push( new THREE.Vector3( 1, 2,-1 ) ); //Vertice no.5
+//forma.vertices.push( new THREE.Vector3(-1, 2,-1 ) ); //Vertice no.6
+//forma.vertices.push( new THREE.Vector3(-1, 2, 1 ) ); //Vertice no.7
+
+
+//forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); //Cara formada por tres vertices
+//forma.faces.push( new THREE.Face3( 3, 1, 0 ) );
+//forma.faces.push( new THREE.Face3( 7, 6, 5 ) );
+//forma.faces.push( new THREE.Face3( 7, 5, 4 ) );
+//forma.faces.push( new THREE.Face3( 5, 4, 1 ) );
+//forma.faces.push( new THREE.Face3( 6, 5, 2 ) );
 
 
 forma.computeBoundingSphere(); //Esfera de menor tamaño que acota al objeto
