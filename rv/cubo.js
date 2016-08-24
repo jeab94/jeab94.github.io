d378c051beee6ac0929863e52d1b8ba1e56a1c16
jeab94 <123456789manu@hotmail.es>
2016-08-24 14:09:13 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -1,13 +1,13 @@
 var forma = new THREE.Geometry(); //Nueva variable tipo Geometría
 
-forma.vertices.push(new THREE.Vector3(1,0,1)); //Enfocamos un valor de  un vector de 3 Vector3(x0,y0,z0) Vertice no.0 
-forma.vertices.push(new THREE.Vector3(1,0,-1)); //Vertice no.1
-forma.vertices.push(new THREE.Vector3(-1,0,-1)); //Vertice no.2
-forma.vertices.push(new THREE.Vector3(-1,0,1)); //Vertice no.3
-forma.vertices.push(new THREE.Vector3(0,1,0)); //Vertice no.4
+forma.vertices.push( new THREE.Vector3( 1, 0, 1 ) ); //Enfocamos un valor de  un vector de 3 Vector3(x0,y0,z0) Vertice no.0 
+forma.vertices.push( new THREE.Vector3( 1, 0,-1 ) ); //Vertice no.1
+forma.vertices.push( new THREE.Vector3(-1, 0,-1 ) ); //Vertice no.2
+forma.vertices.push( new THREE.Vector3(-1, 0, 1 ) ); //Vertice no.3
+forma.vertices.push( new THREE.Vector3( 0, 1, 0 ) ); //Vertice no.4
 
-forma.faces.push(new THREE.Face3(3,1,2)); //Cara formada por tres vertices
-forma.faces.push(new THREE.Face3(3,0,1));
+forma.faces.push( new THREE.Face3( 3, 1, 2 ) ); //Cara formada por tres vertices
+forma.faces.push( new THREE.Face3( 3, 0, 1 ) );
 
 //forma.faces.push( new THREE.Face3( 0, 4, 1 ) );
 //forma.faces.push( new THREE.Face3( 1, 4, 5 ) );
