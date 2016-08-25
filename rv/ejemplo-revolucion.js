f2726325993427b2bb9e4d6412dfe717804152af
jeab94 <123456789manu@hotmail.es>
2016-08-25 15:50:01 -0500

    Malla por Revolución

--- rv/ejemplo-revolucion.js
+++ rv/ejemplo-revolucion.js
@@ -10,3 +10,13 @@ var material = new THREE.MeshNormalMaterial();
 
 var malla = new THREE.Mesh( forma, material );
 malla.rotateX( Math.PI/6 );
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
