e37257f8b4379e00976667f378eda784dd4770af
jeab94 <123456789manu@hotmail.es>
2016-08-25 15:07:28 -0500

    Update ejemplo-figura.js

--- rv/ejemplo-figura.js
+++ rv/ejemplo-figura.js
@@ -6,7 +6,7 @@ figura.lineTo( 10, 40 );
 figura.lineTo( 40, 40 );
 figura.lineTo( 10, 10 );
 
-var forma = new THRE.ShapeGeometry(figura); //Agregamos plano a espacio 3D
+var forma = new THREE.ShapeGeometry(figura); //Agregamos plano a espacio 3D
 var malla = new THREE.Mesh(forma); //El material por default es MeshBasicMaterial
 
 var escena = new THREE.Scene();
