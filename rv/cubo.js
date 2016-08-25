42e6cae035047199d639d65958f3162d0a5691e7
jeab94 <123456789manu@hotmail.es>
2016-08-25 08:51:49 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -17,8 +17,8 @@ forma.faces.push( new THREE.Face3( 4, 1, 0 ) );
 forma.faces.push( new THREE.Face3( 4, 5, 1 ) );
 forma.faces.push( new THREE.Face3( 2, 5, 6 ) );
 forma.faces.push( new THREE.Face3( 2, 1, 5) );
-//forma.faces.push( new THREE.Face3( 7, 5, 6 ) );
-//forma.faces.push( new THREE.Face3( 7, 4, 5 ) );
+forma.faces.push( new THREE.Face3( 3, 2, 7 ) );
+forma.faces.push( new THREE.Face3( 2, 6, 7 ) );
 //forma.faces.push( new THREE.Face3( 3, 4, 7 ) );
 //forma.faces.push( new THREE.Face3( 3, 0, 4 ) );
 
