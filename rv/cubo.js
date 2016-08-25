87553b87524ae136a1a76649bb8093e17b4e384c
jeab94 <123456789manu@hotmail.es>
2016-08-25 08:53:20 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -19,8 +19,8 @@ forma.faces.push( new THREE.Face3( 2, 5, 6 ) );
 forma.faces.push( new THREE.Face3( 2, 1, 5) );
 forma.faces.push( new THREE.Face3( 3, 2, 7 ) );
 forma.faces.push( new THREE.Face3( 2, 6, 7 ) );
-//forma.faces.push( new THREE.Face3( 3, 4, 7 ) );
-//forma.faces.push( new THREE.Face3( 3, 0, 4 ) );
+forma.faces.push( new THREE.Face3( 3, 4, 7 ) );
+forma.faces.push( new THREE.Face3( 3, 0, 4 ) );
 
 forma.computeBoundingSphere(); //Esfera de menor tamaño que acota al objeto
 forma.computeFaceNormals(); //Se computan las normales de las caras
