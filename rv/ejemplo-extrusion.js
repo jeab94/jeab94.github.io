6612e7bbb9fb179911c6d1f551b5d020cacfef0a
jeab94 <123456789manu@hotmail.es>
2016-08-25 15:30:05 -0500

    Update and rename ejemplo-extrusion to ejemplo-extrusion.js

--- rv/ejemplo-extrusion.js
+++ rv/ejemplo-extrusion.js
@@ -13,3 +13,14 @@ var forma = new THREE.ExtrudeGeometry( figura, {amount: 10} ); //amount-->Propie
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( forma, material );
 malla.rotateX( Math.PI/4 );
+
+var escena = new THREE.Scene();
+escena.add(malla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 500;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setsize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara);
