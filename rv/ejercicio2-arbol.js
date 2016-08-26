91e5cda02ffc00e00cb0584db6b1bb39587e8d4f
jeab94 <123456789manu@hotmail.es>
2016-08-26 09:24:49 -0500

    Update ejercicio2-arbol.js

--- rv/ejercicio2-arbol.js
+++ rv/ejercicio2-arbol.js
@@ -11,7 +11,7 @@ var tetraedro = new THREE.Mesh( forma1, material1 ); //Cosntructor Mesh()
 tetraedro.rotateX(-Math.PI/4); //Rota 45° en X
 tetraedro.rotateY(-Math.PI/4); //Rota 45° en Y
 tetraedro.translateY(1);
-tetraedro.translateX(0.5);
+tetraedro.translateZ(0.5);
 
 var forma2 = new THREE.CylinderGeometry( 0.25, 0.5, 2, 10 );
 var material2 = new THREE.MeshBasicMaterial({ color: 0x993300 });
