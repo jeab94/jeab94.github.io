c337cd0797d3ae3228b6d3fcdac4c04ba9e242b0
jeab94 <123456789manu@hotmail.es>
2016-08-26 09:00:05 -0500

    Update ejercicio2-arbol.js

--- rv/ejercicio2-arbol.js
+++ rv/ejercicio2-arbol.js
@@ -7,14 +7,14 @@ document.body.appendChild(renderizador.domElement); //Inserta un nodo al Body
 
 var forma1 = new THREE.TetrahedronGeometry(1,0);
 var material1 = new THREE.MeshNormalMaterial(); //el de default sería MeshBasicMaterial
-var tetraedro = new THREE.Mesh(forma1,material1); //Cosntructor Mesh()
+var tetraedro = new THREE.Mesh( forma1, material1 ); //Cosntructor Mesh()
 tetraedro.rotateX(-Math.PI/4); //Rota 45° en X
 tetraedro.rotateY(-Math.PI/4); //Rota 45° en Y
 escena.add(tetraedro);
 
-var geometry = new THREE.CylinderBufferGeometry( 1, 1, 50, 10 );
-var material2 = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
-var cylinder = new THREE.Mesh( geometry, material2 );
-escena.add( cylinder );
+var forma2 = new THREE.CylinderGeometry( 2, 4, 6, 32 );
+var material2 = new THREE.MeshBasicMaterial({ color: 0x993300 });
+var cilindro = new THREE.Mesh( forma2, material2 );
+escena.add( cilindro );
 
 renderizador.render(escena,camara);
