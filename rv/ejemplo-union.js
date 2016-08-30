f55217053c51b0e0f4832b7100da135305b2dd8e
jeab94 <123456789manu@hotmail.es>
2016-08-30 15:04:05 -0500

    Update ejemplo-union.js

--- rv/ejemplo-union.js
+++ rv/ejemplo-union.js
@@ -5,7 +5,7 @@ esferaForma.translate(0,1,0);
 var troncoMalla = new THREE.Mesh(troncoForma);
 var esferaMalla = new THREE.Mesh(esferaForma); //La malla calcula la matriz de la cinemática del objeto
 
-var arbolForma = new THREE.Geometrry();
+var arbolForma = new THREE.Geometry();
 arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix); //La malla calcula la matriz de la cinemática del objeto
 arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix); //La malla calcula la matriz de la cinemática del objeto
 
