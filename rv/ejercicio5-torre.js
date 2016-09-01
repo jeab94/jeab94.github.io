453cd6d368030d2970b5c62bb5c7922a93f37ad0
jeab94 <123456789manu@hotmail.es>
2016-09-01 15:06:28 -0500

    Update ejercicio5-torre.js

--- rv/ejercicio5-torre.js
+++ rv/ejercicio5-torre.js
@@ -26,10 +26,27 @@ var material2 = new THREE.MeshNormalMaterial();
 var malla2 = new THREE.Mesh( torre2, material2 );
 //malla2.rotateZ( Math.PI/6 );
 
+//Terminación de la torre
+var termino = new THREE.Shape();
+
+var puntos2 = [];
+
+for ( var j=1; j=100; i++ ){
+  puntos2.push( new THREE.Vector2( 50, (j-5)*2));
+}
+
+var torre3 = new THREE.LatheGeometry(puntos2);
+
+var material3 = new THREE.MeshNormalMaterial();
+
+var malla3 = new THREE.Mesh( torre3, material3 );
+
+
 //Unión
 var torreForma = new THREE.Geometry();
 torreForma.merge(malla1.geometry, malla1.matrix); //La malla calcula la matriz de la cinemática del objeto
 torreForma.merge(malla2.geometry, malla2.matrix); //La malla calcula la matriz de la cinemática del objeto
+torreForma.merge(malla3.geometry, malla3.matrix);
 
 var material = new THREE.MeshNormalMaterial();
 var mallaTorre = new THREE.Mesh(torreForma, material);
