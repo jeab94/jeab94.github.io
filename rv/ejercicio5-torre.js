c5a3d4fa0f2b5cd4cae7e302e6e22559a706b667
jeab94 <123456789manu@hotmail.es>
2016-09-01 15:40:01 -0500

    Update ejercicio5-torre.js

--- rv/ejercicio5-torre.js
+++ rv/ejercicio5-torre.js
@@ -57,6 +57,7 @@ escena.add(mallaTorre);
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 500;
+camara.position.x = -500;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
