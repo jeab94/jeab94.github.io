5c66f5ee4c13c98a6dff139c6b19610a1d25990e
jeab94 <123456789manu@hotmail.es>
2016-09-01 14:57:44 -0500

    Update ejercicio5-torre.js

--- rv/ejercicio5-torre.js
+++ rv/ejercicio5-torre.js
@@ -15,7 +15,7 @@ malla1.translateZ( -50 );
 //Mitad de la torre
 var puntos = [];
 
-for( var i = 0; i < 20; i++ ){ //Son 50 puntos
+for( var i = 0; i < 100; i++ ){ //Son 50 puntos
   puntos.push( new THREE.Vector2( Math.sin(i*0.1)*15+50, (i-5)*2) );
 }
 
