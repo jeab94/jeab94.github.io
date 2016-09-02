f60531acdf133e411f82d9ce0d09a70023ce0983
jeab94 <123456789manu@hotmail.es>
2016-09-02 00:17:03 -0500

    Update ejercicio5-torre.js

--- rv/ejercicio5-torre.js
+++ rv/ejercicio5-torre.js
@@ -31,8 +31,8 @@ var malla2 = new THREE.Mesh( torre2, material2 );
 //Terminación de la torre
 var puntos2 = [];
 
-for ( var j = 0; j < 100 ; j++ ){
-  puntos2.push( new THREE.Vector2( 50, (j-5)*2));
+for ( var j = 100; j > 50 ; j-- ){
+  puntos2.push( new THREE.Vector2( j-50, (j-5)*2));
 }
 
 var torre3 = new THREE.LatheGeometry(puntos2);
