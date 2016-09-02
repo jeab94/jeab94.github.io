db6e7d250545fc5d3be6b6c8c61b691a819dabe5
jeab94 <123456789manu@hotmail.es>
2016-09-01 23:59:41 -0500

    Update ejercicio5-torre.js

--- rv/ejercicio5-torre.js
+++ rv/ejercicio5-torre.js
@@ -8,7 +8,7 @@ base.lineTo( 40, -10 );
 base.lineTo( -40, -10 );
 
 var torre1 = new THREE.ExtrudeGeometry(base, { amount:100 } );
-torre1.translateZ( 1600 );
+torre1.translate(0, 0, 1600);
 var material1 = new THREE.MeshNormalMaterial();
 var malla1 = new THREE.Mesh(torre1, material1);
 
