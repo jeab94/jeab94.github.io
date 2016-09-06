7ba59b29ff4ac377e502879843a5708b7e0abf49
jeab94 <123456789manu@hotmail.es>
2016-09-06 14:54:18 -0500

    Update ejemplo_proyeccion.js

--- rv/ejemplo_proyeccion.js
+++ rv/ejemplo_proyeccion.js
@@ -16,3 +16,14 @@ esfera1.position.x = 5;
 var esfera2 = new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
 esfera2.position.x = -5;
 esfera2.position.z = -10;
+
+var escena = new THREE.Scene();
+escena.add(esfera1);
+escena.add(esfera2);
+escena.add(cubo);
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
