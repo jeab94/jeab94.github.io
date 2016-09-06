06ab8e8ece0a5c24a0432e67b940b2817093468a
jeab94 <123456789manu@hotmail.es>
2016-09-06 15:33:59 -0500

    Update ejemplo_ortografica.js

--- rv/ejemplo_ortografica.js
+++ rv/ejemplo_ortografica.js
@@ -5,7 +5,7 @@ camara.right = window.innerWidth/2;
 camara.top = window.innerHeight/2;
 camara.bottom = window.innerHeight/-2;
 camara.near = 0.1;
-camara.far = 1000;
+camara.far = 100;
 camara.updateProjectionMatrix(); //Actualiza la matriz de proyección
 
 camara.position.z = 100;
