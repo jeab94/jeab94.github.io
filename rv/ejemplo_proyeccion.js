ad940b6fc44c86282f2bd6415b825b2bd7f68e72
jeab94 <123456789manu@hotmail.es>
2016-09-06 14:50:18 -0500

    Create ejemplo_proyeccion.js

---
+++ rv/ejemplo_proyeccion.js
@@ -0,0 +1,18 @@
+var campoVision = 45; //grados
+var relacionAspecto = window.innerWidth/innerHeight;
+var planoCercano = 1;
+var planoLejano = 1000;
+
+var camara = new THREE.PerspectiveCamera(campoVision, relacionAspecto,planoCercano,planoLejano);
+
+camara.position.z = 15;
+
+var cubo = new THREE.Mesh( new THREE.BoxGeometry(2,2,2), new THREE.MeshNormalGeometry() );
+cubo.rotateY(Math.PI/4);
+
+var esfera1 = new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
+esfera1.position.x = 5;
+
+var esfera2 = new THREE.Mesh( new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
+esfera2.position.x = -5;
+esfera2.position.z = -10;
