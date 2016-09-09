9957a63feb67ddced19fbf9ab25dba93d348899f
jeab94 <123456789manu@hotmail.es>
2016-09-09 11:25:37 -0500

    Update ejercicio6_tablero.js

--- rv/ejercicio6_tablero.js
+++ rv/ejercicio6_tablero.js
@@ -32,14 +32,14 @@ for (var j=0;j<=7;j++){
 }
 
 //Unión
-for (i=1; i<64;i++){
-    cubos[0].add(cubos[i]); //
+//for (i=1; i<64;i++){
+  //  cubos[0].add(cubos[i]); //¿?
     
-}
+//}
 
 var escena = new THREE.Scene();
 for (i=1; i<64;i++){
-    escena.add(cubos[i]);
+    escena.add(cubos[i]); //¿?
 
 }
 
