eae40df5f21e0d7aee17783f2d9f2f403dbd24a1
jeab94 <123456789manu@hotmail.es>
2016-09-13 15:04:23 -0500

    Update ejemplo-lambertMaterial.js

--- rv/ejemplo-lambertMaterial.js
+++ rv/ejemplo-lambertMaterial.js
@@ -1,9 +1,15 @@
+var luzPuntual = new THREE.PointLight(0xFFFFFF);
+luzPuntual.position.x = 10;
+luzPuntual.position.y = 10;
+luzPuntual.position.z = 10;
+
 var forma = new THREE.SphereGeometry( 1 );
 var material = new THREE.MeshLambertMaterial({color: '#00cc00'});
 var malla = new THREE.Mesh(forma, material);
 
 var escena = new THREE.Scene();
 escena.add(malla);
+escena.add(luzPuntual);
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
