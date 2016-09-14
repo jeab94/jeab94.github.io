78cb70eba5e28e9cfa89c41651ccb4cba4e35307
jeab94 <123456789manu@hotmail.es>
2016-09-14 09:46:15 -0500

    Update ejercicio6_tablero.js

--- rv/ejercicio6_tablero.js
+++ rv/ejercicio6_tablero.js
@@ -1,8 +1,14 @@
-var camara = new THREE.PerspectiveCamera();
-camara.position.y = 0;
-camara.position.z = 250;
-camara.position.x = 40;
-camara.lookAt(new THREE.Vector3(40,0,0));
+var camara = new THREE.OrthographicCamera();
+
+camara.left = window.innerWidth/-2;
+camara.right = window.innerWidth/2;
+camara.top = window.innerHeight/2;
+camara.bottom = window.innerHeight/-2;
+camara.near = 0.1;
+camara.far = 1000;
+camara.updateProjectionMatrix(); //Actualiza la matriz de proyección
+
+camara.position.z = 150;
 
 var grayColor = new THREE.Color("rgb(128,128,128)");
 var whiteColor = new THREE.Color("rgb(255,255,255)");
