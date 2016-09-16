f3c392e65d83e430e42a10aa92380a949e125634
jeab94 <123456789manu@hotmail.es>
2016-09-16 16:24:35 -0500

    Update ejercicio6_tablero.js

--- rv/ejercicio6_tablero.js
+++ rv/ejercicio6_tablero.js
@@ -4,7 +4,7 @@ camara.left = window.innerWidth/-2;
 camara.right = window.innerWidth/2;
 camara.top = window.innerHeight/2;
 camara.bottom = window.innerHeight/-2;
-camara.near = 0.1;
+camara.near = 0.01;
 camara.far = 1000;
 camara.updateProjectionMatrix(); //Actualiza la matriz de proyección
 
