d248f93996e19c3a6f6da98050f90c576fbccfee
jeab94 <123456789manu@hotmail.es>
2016-09-16 20:15:48 -0500

    Update ejercicio6_tablero.js

--- rv/ejercicio6_tablero.js
+++ rv/ejercicio6_tablero.js
@@ -39,12 +39,12 @@ for (var j=0;j<=7;j++){
     }
 }
 
-var orilla1 = new THREE.BoxGeometry( 80, 10, 5 );
+var orilla1 = new THREE.BoxGeometry( 82, 10, 5 );
 var material1 = materialCafe;
 var marco1 = new THREE.Mesh( orilla1, material1 );
 //marco1.translateY(-2.5);
 marco1.translateZ(-2.5);
-//marco1.translateX(31.5);
+marco1.translateX(41);
 
 //var orilla1 = new THREE.Shape();
 //orilla1.moveTo(-2,-5);
