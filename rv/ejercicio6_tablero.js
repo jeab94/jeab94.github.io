ef3d0a1f43118dd6db2a2379bcea1133efd40452
jeab94 <123456789manu@hotmail.es>
2016-09-16 19:40:06 -0500

    Update ejercicio6_tablero.js

--- rv/ejercicio6_tablero.js
+++ rv/ejercicio6_tablero.js
@@ -1,7 +1,7 @@
 var camara = new THREE.PerspectiveCamera();
 
 camara.position.y = 0;
-camara.position.x = 400;
+camara.position.x = 200;
 camara.position.z = 0;
 camara.lookAt(new THREE.Vector3(0,0,40));
 
@@ -38,48 +38,53 @@ for (var j=0;j<=7;j++){
     }
 }
 
-var orilla1 = new THREE.Shape();
-orilla1.moveTo(-2,-5);
-orilla1.lineTo(82,-5);
-orilla1.lineTo(82,5);
-orilla1.lineTo(-2,5);
+var orilla1 = new THREE.BoxGeometry( 84, 10, 2 );
+var material1 = materialCafe;
+var marco1 = new THREE.Mesh( orilla1, material1 );
+marco1.translateZ(-10);
 
-var forma1 = new THREE.ExtrudeGeometry(orilla1, {amount: 2});
-var material2 = materialCafe;
-var marco1 = new THREE.Mesh(forma1, material2);
-marco1.translateZ(-5);
+//var orilla1 = new THREE.Shape();
+//orilla1.moveTo(-2,-5);
+//orilla1.lineTo(82,-5);
+//orilla1.lineTo(82,5);
+//orilla1.lineTo(-2,5);
 
-var orilla2 = new THREE.Shape();
-orilla2.moveTo(80,-5);
-orilla2.lineTo(82,-5);
-orilla2.lineTo(82,5);
-orilla2.lineTo(80,5);
+//var forma1 = new THREE.ExtrudeGeometry(orilla1, {amount: 2});
+//var material2 = materialCafe;
+//var marco1 = new THREE.Mesh(forma1, material2);
+//marco1.translateZ(-5);
 
-var forma2 = new THREE.ExtrudeGeometry(orilla2, {amount: 80});
-var material2 = materialCafe;
-var marco2 = new THREE.Mesh(forma2, material2);
+//var orilla2 = new THREE.Shape();
+//orilla2.moveTo(80,-5);
+//orilla2.lineTo(82,-5);
+//orilla2.lineTo(82,5);
+//orilla2.lineTo(80,5);
+
+//var forma2 = new THREE.ExtrudeGeometry(orilla2, {amount: 80});
+//var material2 = materialCafe;
+//var marco2 = new THREE.Mesh(forma2, material2);
 //marco2.translateZ();
 
-var orilla3 = new THREE.Shape();
-orilla3.moveTo(-2,-5);
-orilla3.lineTo(82,-5);
-orilla3.lineTo(82,5);
-orilla3.lineTo(-2,5);
+//var orilla3 = new THREE.Shape();
+//orilla3.moveTo(-2,-5);
+//orilla3.lineTo(82,-5);
+//orilla3.lineTo(82,5);
+//orilla3.lineTo(-2,5);
 
-var forma3 = new THREE.ExtrudeGeometry(orilla3, {amount: 2 });
-var material3 = materialCafe;
-var marco3 = new THREE.Mesh(forma3, material3);
-marco3.translateZ(80);
+//var forma3 = new THREE.ExtrudeGeometry(orilla3, {amount: 2 });
+//var material3 = materialCafe;
+//var marco3 = new THREE.Mesh(forma3, material3);
+//marco3.translateZ(80);
 
-var orilla4 = new THREE.Shape();
-orilla4.moveTo(-2,-5);
-orilla4.lineTo(0,-5);
-orilla4.lineTo(0,5);
-orilla4.lineTo(-2,5);
+//var orilla4 = new THREE.Shape();
+//orilla4.moveTo(-2,-5);
+//orilla4.lineTo(0,-5);
+//orilla4.lineTo(0,5);
+//orilla4.lineTo(-2,5);
 
-var forma4 = new THREE.ExtrudeGeometry(orilla4, {amount: 80})
-var material4 = materialCafe;
-var marco4 = new THREE.Mesh(forma4, material4);
+//var forma4 = new THREE.ExtrudeGeometry(orilla4, {amount: 80})
+//var material4 = materialCafe;
+//var marco4 = new THREE.Mesh(forma4, material4);
 //marco.translateZ();
 
 var escena = new THREE.Scene();
