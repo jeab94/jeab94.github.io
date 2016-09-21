c265c487deaa964c365f49d1f1045a23f870c3a5
jeab94 <123456789manu@hotmail.es>
2016-09-21 10:19:35 -0500

    Update ejercicio6_tablero.js

--- rv/ejercicio6_tablero.js
+++ rv/ejercicio6_tablero.js
@@ -10,16 +10,6 @@ luzPuntual1.position.x = 40;
 luzPuntual1.position.y = 100;
 luzPuntual1.position.z = 40;
 
-var luzPuntual2 = new THREE.PointLight(0xFFFFFF,0.8);
-luzPuntual2.position.x = 130;
-luzPuntual2.position.y = 100;
-luzPuntual2.position.z = -50;
-
-var luzPuntual3 = new THREE.PointLight(0xFFFFFF,0.8);
-luzPuntual3.position.x = 130;
-luzPuntual3.position.y = 100;
-luzPuntual3.position.z = 130;
-
 var materialBlanco = new THREE.MeshBasicMaterial( { 
     color: 0xffffff
 } );
@@ -27,28 +17,6 @@ var materialBlanco = new THREE.MeshBasicMaterial( {
 var materialCafe = new THREE.MeshBasicMaterial( { 
     color: 0x654321 
 } );
-
-var materialTorre1 = new THREE.MeshBasicMaterial( {
-    color: 0xffffff,
-    //opacity: 1
-} );
-//materialTorre1.opacity = 1;
-var materialTorre2 = new THREE.MeshBasicMaterial( {
-    color: 0xffffff,
-    //opacity: 0.75
-} );
-//materialTorre2.opacity = 0.75;
-var materialTorre3 = new THREE.MeshBasicMaterial( {
-    color: 0x000000,
-    //opacity: 0.5
-} );
-//materialTorre3.opacity = 0.5;
-var materialTorre4 = new THREE.MeshBasicMaterial( {
-    color: 0x000000,
-    //opacity: 0.25
-} );
-//materialTorre4.opacity = 0.25;
-        
 
 //Tablero
 var lado = 10;
@@ -97,11 +65,6 @@ var marco4 = new THREE.Mesh( orilla4, material4);
 marco4.translateZ(40);
 marco4.translateX(-2.5);
 
-marco1.receiveShadow = true;
-marco2.receiveShadow = true;
-marco3.receiveShadow = true;
-marco4.receiveShadow = true;
-
 //Torre1Blanca
     torre1 = mallaTorre.clone();
     torre1.scale.set(0.07,0.07,0.07);
@@ -109,7 +72,6 @@ marco4.receiveShadow = true;
     torre1.translateX(5);
     torre1.translateZ(5);
     torre1.material = materialBlanco;
-    torre1.castShadow = true;
     
 //Torre2Blanca
     torre2 = mallaTorre.clone();
@@ -118,7 +80,6 @@ marco4.receiveShadow = true;
     torre2.translateX(5);
     torre2.translateZ(75);
     torre2.material = materialBlanco;
-    torre2.castShadow = true;
     
 //Torre3Negra
     torre3 = mallaTorre.clone();
@@ -126,17 +87,14 @@ marco4.receiveShadow = true;
     torre3.translateY(10);
     torre3.translateX(75);
     torre3.translateZ(5);
-    torre3.castShadow = true;
-
+    
 //Torre4Negra
     torre4 = mallaTorre.clone();
     torre4.scale.set(0.07,0.07,0.07);
     torre4.translateY(10);
     torre4.translateX(75);
     torre4.translateZ(75);
-    torre4.castShadow = true;
     
-
 var escena = new THREE.Scene();
 escena.add(marco1);
 escena.add(marco2);
@@ -146,21 +104,15 @@ escena.add(torre1);
 escena.add(torre2);
 escena.add(torre3);
 escena.add(torre4);
-escena.add(luzPuntual1, luzPuntual2, luzPuntual3);
+escena.add(luzPuntual1);
 
 
 for (i = 0; i < 64; i++) {
-cubos[i].receiveShadow = true;
 escena.add(cubos[i]);
 }
 
-
 var renderizador = new THREE.WebGLRenderer({});
 renderizador.setSize(window.innerWidth, window.innerHeight); //Renderizador en toda la pantalla
-renderizador.shadowMapEnabled=true;
-luzPuntual1.castShadow = true;
-luzPuntual2.castShadow = true;
-luzPuntual3.castShadow = true;
 
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena, camara);
