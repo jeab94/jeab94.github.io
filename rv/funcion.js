bd4bf53abd36acd6ede83f318a45b93160b693c6
jeab94 <123456789manu@hotmail.es>
2016-09-22 15:06:56 -0500

    Create funcion.js

---
+++ rv/funcion.js
@@ -0,0 +1,20 @@
+function init(p){
+  var malla = new THREE.Mesh(new THREE.BoxGeometry(p,p,p), new THREE.MeshNormalMaterial());
+  escena = new THREE.Scene(); //Sin var son variables globales
+  escena.add(malla);
+  
+  camara = new THREE.PerspectiveCamera();
+  
+  renderizador = new THREE.WebGLRenderer(); //Sin var son variables globales
+  renderizador.setSize(100, 700);
+  document.body.appendChild(renderizador.domElement);
+}
+
+var main = function (p){
+  renderizador.renderer(escena, camara);
+}
+
+var escena, camara, renderizador;
+
+init(1);
+main(2);
