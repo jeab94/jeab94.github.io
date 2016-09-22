1ff509163da8af6d33fc6620d687df749e6ab1c4
jeab94 <123456789manu@hotmail.es>
2016-09-21 23:04:17 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -120,7 +120,7 @@ escena.add(torre1);
 escena.add(torre2);
 escena.add(torre3);
 escena.add(torre4);
-escena.add(luzPuntual1);
+escena.add(luzPuntual1,luzPuntual2, luzPuntual3);
 
 
 for (i = 0; i < 64; i++) {
