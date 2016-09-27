da216abda41e33353b2ce233ace8b77933b11e84
jeab94 <123456789manu@hotmail.es>
2016-09-27 14:57:42 -0500

    Update ejemplo_cambio_tamaño.js

--- rv/ejemplo_cambio_tamaño.js
+++ rv/ejemplo_cambio_tamaño.js
@@ -1,12 +1,12 @@
 var VENTANA = new Object(); //Módulo
 
-VENTANA.listener = fucntion(){
+VENTANA.listener = function(){
   VENTANA.camara.aspect = window.innerWidth/window.innerHeight;
   VENTANA.camara.updateProjectMatrix();
   VENTANA.renderizador.setSize(window.innerWidth, window.innerHeight);
   }
   
- VENTANA.setup = fucntion(){
+ VENTANA.setup = function(){
    var tipo_evento = 'resize';
    var capturarp = false;
    window.addEventListener(tipo_evento, VENTANA.listener, capturarp);
