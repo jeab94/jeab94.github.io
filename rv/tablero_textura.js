5d3c1c7bda8dcef596f4a1643055398ecdba13a3
jeab94 <123456789manu@hotmail.es>
2016-09-29 15:10:05 -0500

    Update tablero_textura.js

--- rv/tablero_textura.js
+++ rv/tablero_textura.js
@@ -5,13 +5,13 @@ camara.position.x = 100;
 camara.position.z = 100;
 camara.lookAt(new THREE.Vector3(40,5,40));
 
-var luzPuntual1 = new THREE.PointLight(0xFFFFFF);
+var luzPuntual1 = new THREE.PointLight(0xFFFFFF, 0.5);
 luzPuntual1.position.set(0,100,0);
 
-var luzPuntual2 = new THREE.PointLight(0xFFFFFF);
+var luzPuntual2 = new THREE.PointLight(0xFFFFFF,0.5);
 luzPuntual2.position.set(80,100,0);
 
-var luzPuntual3 = new THREE.PointLight(0xFFFFFF);
+var luzPuntual3 = new THREE.PointLight(0xFFFFFF,0.5);
 luzPuntual3.position.set(0,100,80);
 
 whiteColor = new THREE.Color(0xffffff);
