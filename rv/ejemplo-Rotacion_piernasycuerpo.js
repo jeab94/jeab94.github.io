1c6571c9dbbbaed267e0cf31faa55e8d3012da9c
jeab94 <123456789manu@hotmail.es>
2016-10-20 14:59:30 -0500

    Create ejemplo-Rotacion_piernasycuerpo.js

---
+++ rv/ejemplo-Rotacion_piernasycuerpo.js
@@ -0,0 +1,40 @@
+fucntion Pieza(){
+  THREE.Object3D.call(this);
+  var piernaIzq = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
+  var piernaDer = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
+  var cuerpo = new THREE.Mesh(new THREE.BoxGeometry(5,10,5));
+  this.add(piernaIzq,piernaDer,cuerpo);
+  this.piernaIzq.position.z = -2;
+  this.piernaIzq.position.y = -2.5;
+  piernaDer.position.z = 2;
+  piernaDer.position.y = -2.5;
+  cuerpo.position.z = 2.5;
+}
+
+Pieza.prototype = new THREE.Object3D;
+function setup(){
+  pieza = new Pieza();
+  
+  camara = new THREE.PerspectiveCamera();
+  camara.position.z = 20;
+  
+  var lienzo = document.getElementById("ejemplo-Rotacion_piernasycuerpo");
+  renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+  renderizador.setSize(600,600);
+  
+  escena = new THREE.Scene();
+  escena.add(pieza);
+  escena.add(arbol2.malla);
+}
+
+function loop(){
+  pieza.rotateY = 0.1;
+}
+
+loop = function(){
+  requestAnimationFrame(loop); 
+ renderizador.render(escena, camara);
+}
+
+setup();
+loop();
