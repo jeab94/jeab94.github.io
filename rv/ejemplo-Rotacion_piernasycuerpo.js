e388e5567d6b09ccfa515d8702f706d7da1fc598
jeab94 <123456789manu@hotmail.es>
2016-10-20 15:08:04 -0500

    Update ejemplo-Rotacion_piernasycuerpo.js

--- rv/ejemplo-Rotacion_piernasycuerpo.js
+++ rv/ejemplo-Rotacion_piernasycuerpo.js
@@ -1,14 +1,14 @@
 function Pieza(){
+  
   THREE.Object3D.call(this);
   var piernaIzq = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
   var piernaDer = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
   var cuerpo = new THREE.Mesh(new THREE.BoxGeometry(5,10,5));
   this.add(this.piernaIzq,this.piernaDer,this.cuerpo);
-  this.piernaIzq.position.z = -2;
-  this.piernaIzq.position.y = -2.5;
-  piernaDer.position.z = 2;
-  piernaDer.position.y = -2.5;
-  cuerpo.position.z = 2.5;
+  this.piernaIzq.position.set(0, -2.5,-2)
+  this.piernaDer.position.set(0, -2.5, 2)
+  this.cuerpo.position.set(0, 0, 2.5)
+  
 }
 
 Pieza.prototype = new THREE.Object3D;
