1b53d4044f65500d391a87a78df1aba00bef525a
jeab94 <123456789manu@hotmail.es>
2016-10-25 15:37:10 -0500

    Update ejemplo-Agentes.js

--- rv/ejemplo-Agentes.js
+++ rv/ejemplo-Agentes.js
@@ -66,7 +66,7 @@ Pelota.prototype.sense = function(environment){
 }
 
 Pelota.prototype.act = function(environment){
-  if (this.colision === 1
+  if (this.colision === 1)
       this.step = -this.step;
   this.position.x += this.step;
 }
