91aea0221bf0eae0ad43c3b9f08e17be5b5dd640
jeab94 <123456789manu@hotmail.es>
2016-10-27 15:36:06 -0500

    Update ejemplo-robot.js

--- rv/ejemplo-robot.js
+++ rv/ejemplo-robot.js
@@ -130,6 +130,37 @@ function Robot(size, x, y){
  this.add(this.actuator);
 }
 
+Robot.prototype = new Agent();
+
+Robot.prototype.sense = function(environment){
+  this.sensor.set(this.position, new THREE.Vector3(Math.cos(this.rotation.z), Math.sin(this.rotation.z), 0));
+  var obstaculo = this.sensor.intersectObjects(environment.children, true);
+  
+  if((obstaculo.length>0 && (obstaculo[0].distance<=0.5)))
+    this.sensor.colision=true;
+  else
+    this.sensor.colision=false;
+}
+
+Robot.prototype.plan = function(environment){
+ this.actuator.commands = [];
+ 
+ if (this.sensor.colision == true)
+   this.actuator.commands.push('rotateCCW');
+ else
+   this.actuator.commands.push('gostraight');
+}
+
+Robot.prototype.act = function(environment){
+  var command = this.actuator.commands.pop();
+  
+  if(command === undefined)
+    console.log('Undefined command')
+  else if(command in this.operations)
+    this.operations[command](this);
+  else
+    console.log('Unknown command');
+}
 
 setup();
 loop();
