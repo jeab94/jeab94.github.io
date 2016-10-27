2d04a213f60bd64fa8ef39e3efa398efcf0ea0b3
jeab94 <123456789manu@hotmail.es>
2016-10-27 15:46:07 -0500

    Update ejemplo-robot.js

--- rv/ejemplo-robot.js
+++ rv/ejemplo-robot.js
@@ -94,7 +94,9 @@ function setup(){
   camera = new THREE.PerspectiveCamera();
   camera.position.z = 30;
   
-  renderer = new THREE.WebGLRenderer();
+  var lienzo = document.getElementById("ejemplo-robot");
+  
+  renderer = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
   renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
   document.body.appendChild(renderer.domElement);
   
@@ -161,6 +163,28 @@ Robot.prototype.act = function(environment){
   else
     console.log('Unknown command');
 }
+
+Robot.prototype.operations = {};
+
+Robot.prototype.operations.goStraight = function(robot, distance){
+  if (distance=== undefined)
+    distance = 0.05;
+  robot.position.x += distance*Math.cos(robot.rotation.z);
+  robot.position.y += distance*Math.sin(robot.rotation.z);
+}
+
+Robot.prototype.operations.rotateCW = function(robot, angle){
+ if (angle===undefined)
+   angle = Math.PI/2;
+  robot.rotation.z -= angle;
+}
+
+Robot.prototype.operations.rotateCCW = function(robot, angle){
+ if (angle===undefined)
+   angle = Math.PI/2;
+  robot.rotation.z += angle;
+}
+
 
 setup();
 loop();
