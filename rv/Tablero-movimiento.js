0ee8bea4c60717da0c92ca2bb7bac15cbcacca0d
jeab94 <123456789manu@hotmail.es>
2016-11-06 23:58:38 -0600

    Update Tablero-movimiento.js

--- rv/Tablero-movimiento.js
+++ rv/Tablero-movimiento.js
@@ -18,7 +18,7 @@ cargadorWhite.load("white_wood.jpg", fnWhite);
 var cargadorWood=new THREE.TextureLoader();
 cargadorWood.load("wood.jpg", fnWood);
 
-var poner=function(x, y, z){
+Environment.prototype.setTablero = function(x, y, z){
 //Tablero
 var lado = 10;
 var cubos = [];
@@ -35,8 +35,9 @@ for (var j=0;j<=7;j++){
         }
     var forma = new THREE.BoxBufferGeometry(lado,lado,lado);
     var cubo = new THREE.Mesh(forma ,material);
-    cubo.position.x = j*lado+5;
-    cubo.position.z = i*lado+5;
+    cubo.position.x = j*lado+5+x;
+    cubo.position.z = i*lado+5+z;
+    cubo.position.y = y;
     cubos.push(cubo)
     
     }
@@ -45,40 +46,27 @@ for (var j=0;j<=7;j++){
 var orilla1 = new THREE.BoxGeometry( 90, 10, 5 );
 var material1 = Marco;
 var marco1 = new THREE.Mesh( orilla1, material1 );
-marco1.translateZ(-2.5);
-marco1.translateX(40);
+marco1.translateZ(-2.5+z);
+marco1.translateX(40+x);
+marco1.translateY(y);
 
 var orilla2 = new THREE.BoxGeometry( 5, 10, 80 );
 var material2 = Marco;
 var marco2 = new THREE.Mesh( orilla2, material2);
-marco2.translateZ(40);
-marco2.translateX(82.5);
+marco2.translateZ(40+z);
+marco2.translateX(82.5+x);
+marco2.translateY(y);
 
 var orilla3 = new THREE.BoxGeometry( 90, 10, 5 );
 var material3 = Marco;
 var marco3 = new THREE.Mesh( orilla3, material3);
-marco3.translateZ(82.5);
-marco3.translateX(40);
+marco3.translateZ(82.5+z);
+marco3.translateX(40+x);
+marco3.translateX(40+x);
 
 var orilla4 = new THREE.BoxGeometry( 5, 10, 80 );
 var material4 = Marco;
 var marco4 = new THREE.Mesh( orilla4, material4);
-marco4.translateZ(40);
-marco4.translateX(-2.5);
-
-escena = new THREE.Scene();
-   
-for (i = 0; i < 64; i++) {
-escena.add(cubos[i]);
-escena.add(marco1);
-escena.add(marco2);
-escena.add(marco3);
-escena.add(marco4);
-
-}
-
-renderizador = new THREE.WebGLRenderer();
-renderizador.setSize(window.innerWidth, window.innerHeight); //Renderizador en toda la pantalla
-
-document.body.appendChild(renderizador.domElement);
+marco4.translateZ(40+z);
+marco4.translateX(-2.5+x);
 }
