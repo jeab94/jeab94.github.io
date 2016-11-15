54e850b20ac6a1a057dccfb526e59f0ec3847138
jeab94 <123456789manu@hotmail.es>
2016-11-15 15:13:50 -0600

    Update Tablero-movimiento.js

--- rv/Tablero-movimiento.js
+++ rv/Tablero-movimiento.js
@@ -120,12 +120,6 @@ function loop(){
          environment.act();
          didSetup = true;
       }
-   for (var m=0; m=100; m++){
-   camera.position.z = m;
-   renderer.render(environment, camera);
-      if (m==100)
-         m=0;
-   }
   //}
    renderer.render(environment, camera);
 }
