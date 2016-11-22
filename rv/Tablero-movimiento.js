e6719f89a3601895f92a61087f11a0311c3b73dc
jeab94 <123456789manu@hotmail.es>
2016-11-22 15:48:33 -0600

    Update Tablero-movimiento.js

--- rv/Tablero-movimiento.js
+++ rv/Tablero-movimiento.js
@@ -76,7 +76,7 @@ setTablero = function(x, y, z){
    tablero.merge(marco3.geometry, marco3.matrix);
    tablero.merge(marco4.geometry, marco4.matrix);
    for(var q=1; q<=64; q++){
-      tablero.merge(cubos[q]);
+      tablero.merge(cubos[q].geometry, cubos[q].matrix);
    }
    
    environment.add(tablero);
