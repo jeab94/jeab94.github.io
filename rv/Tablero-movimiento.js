188c4be8b14847328a64df3a34c195762a26419e
jeab94 <123456789manu@hotmail.es>
2016-11-22 15:02:47 -0600

    Update Tablero-movimiento.js

--- rv/Tablero-movimiento.js
+++ rv/Tablero-movimiento.js
@@ -1,41 +1,31 @@
-//var mat1 = false;
-//var mat2 = false;
-//var mat3 = false;
+var mat1 = false;
+var mat2 = false;
+var mat3 = false;
 
 var environment
 var renderer;
 var camera;
 
-//var fnBlack = function(textura) {
-   //Gris = new THREE.MeshBasicMaterial({map: textura});  
-   //mat1 = true;
-//}
-//var fnWhite = function(textura) {
-   //Blanco = new THREE.MeshBasicMaterial({map: textura});  
-   //mat2 = true;
-//}
-//var fnWood = function(textura) {
-   //Marco = new THREE.MeshBasicMaterial({map: textura});  
-   //mat3 = true;
-//}
-
-//var cargadorBlack=new THREE.TextureLoader();
-//cargadorBlack.load("black_marmol.jpg", fnBlack);
-//var cargadorWhite=new THREE.TextureLoader();
-//cargadorWhite.load("white_marmol.jpg", fnWhite);
-//var cargadorWood=new THREE.TextureLoader();
-//cargadorWood.load("wood.jpg", fnWood);
-
-//var mat1 = true;
-//var mat2 = true;
-//var mat3 = true;
-
-
-
 Environment.prototype.setTablero = function(x, y, z){
-   var Gris = new THREE.MeshBasicMaterial({color:0x696969});
-   var Blanco = new THREE.MeshBasicMaterial({color:0xFFFFFF});
-   var Marco = new THREE.MeshBasicMaterial({color:0x7E2E1F});
+var fnBlack = function(textura) {
+   Gris = new THREE.MeshBasicMaterial({map: textura});  
+   mat1 = true;
+}
+var fnWhite = function(textura) {
+   Blanco = new THREE.MeshBasicMaterial({map: textura});  
+   mat2 = true;
+}
+var fnWood = function(textura) {
+   Marco = new THREE.MeshBasicMaterial({map: textura});  
+   mat3 = true;
+}
+
+var cargadorBlack=new THREE.TextureLoader();
+cargadorBlack.load("black_marmol.jpg", fnBlack);
+var cargadorWhite=new THREE.TextureLoader();
+cargadorWhite.load("white_marmol.jpg", fnWhite);
+var cargadorWood=new THREE.TextureLoader();
+cargadorWood.load("wood.jpg", fnWood);
    
    //Tablero
    var lado = 10;
@@ -122,7 +112,7 @@ function setup(){
 }
 
 function loop(){
-   //if(mat1 && mat2 && mat3){
+   if(mat1 && mat2 && mat3){
      requestAnimationFrame(loop);
          environment.sense();
          environment.plan();
