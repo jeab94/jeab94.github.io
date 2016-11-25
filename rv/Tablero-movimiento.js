a31ab72a89d23a2234a61171190cb0940745c9f3
jeab94 <123456789manu@hotmail.es>
2016-11-25 00:27:28 -0600

    Update Tablero-movimiento.js

--- rv/Tablero-movimiento.js
+++ rv/Tablero-movimiento.js
@@ -112,7 +112,7 @@ function setup(){
     	var caballo = new THREE.Mesh( geometry, material );
     	caballo.position.set( 15, 20, 75 );
     	//caballo.rotation.set( 0, - Math.PI / 2, 0 );
-    	caballo.scale.set( 0.70, 0.70, 0.70 );
+    	caballo.scale.set( 0.50, 0.50, 0.50 );
     	//caballo.castShadow = true;
     	//caballo.receiveShadow = true;
     	escena.add( caballo );
