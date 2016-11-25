c624f8c3a45ecd01c4b8b401065e3a29ae3aee1e
jeab94 <123456789manu@hotmail.es>
2016-11-25 00:29:30 -0600

    Update Tablero-movimiento.js

--- rv/Tablero-movimiento.js
+++ rv/Tablero-movimiento.js
@@ -108,7 +108,7 @@ function setup(){
      // Object
     var loader=new THREE.STLLoader();
     loader.load( './Chess-Pieces/Horse.STL', function ( geometry ) {
-    	var material = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('wood.jpg') });
+    	var material = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('white_marmol.jpg') });
     	var caballo = new THREE.Mesh( geometry, material );
     	caballo.position.set( 15, 20, 75 );
     	//caballo.rotation.set( 0, - Math.PI / 2, 0 );
