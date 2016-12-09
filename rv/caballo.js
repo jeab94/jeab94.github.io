71f92b8fb5a15e698c96d13962970f7d6eba4063
jeab94 <123456789manu@hotmail.es>
2016-12-09 10:24:14 -0600

    Delete caballo.js

--- rv/caballo.js
+++
@@ -1,7 +0,0 @@
-   //CABALLO
-     // Object
-    var caballo;
-    var loader=new THREE.STLLoader();
-    loader.load( './Chess-Pieces/Horse.STL', function ( geometry ) {
-    	caballo = geometry;
-    } );
