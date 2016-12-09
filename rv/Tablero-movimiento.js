3fd596fe3820ffd712ceca7f3bdbf696dca14f1a
jeab94 <123456789manu@hotmail.es>
2016-12-09 13:19:46 -0600

    Update Tablero-movimiento.js

--- rv/Tablero-movimiento.js
+++ rv/Tablero-movimiento.js
@@ -18,11 +18,11 @@ var geometryCaballoNegro;
 var material;
 
 //Texturas
-var Gris = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('black_marmol.jpg') });
-var Blanco = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('white_marmol.jpg') });
-var Marco = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('wood.jpg') });
-var GrisLiso = new THREE.MeshBasicMaterial({color: 0xD3D3D3});
-var BlancoLiso = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
+var Gris = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('black_marmol.jpg') });
+var Blanco = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('white_marmol.jpg') });
+var Marco = new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture('wood.jpg') });
+var GrisLiso = new THREE.MeshLambertMaterial({color: 0xD3D3D3});
+var BlancoLiso = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
 
 //Sensor
 function Sensor(position,direction){ 
@@ -249,7 +249,7 @@ var king = [];
 		loader.load( 'Chess-Pieces/king.dae', function ( collada ) {
 			for (var i = 0; i < 2 ;i++) {
 			   king[i] = collada.scene.clone();
-			   escena.add(new THREE.Mesh(king[0],Gris));
+			   escena.add(new THREE.Mesh(king[0],GrisLiso));
 			}	
 			});
 	
